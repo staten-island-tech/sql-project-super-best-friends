f7527e1d92cce9191d77fd3dd549dd8b589742f1
JasonChiu2 <stemlads@gmail.com>
2023-05-15 10:15:54 -0400

    Added test image

---
+++ supabase-vue-3/src/components/GameCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <img
+      src="https://sm.ign.com/t/ign_ap/deal/d/daily-deal/daily-deals-save-on-zelda-totk-root-a-game-of-woodland-might_d5x8.1280.jpg"
+      alt="URMA"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    title: String,
+    summary: String,
+  },
+};
+</script>
+
+<style></style>
