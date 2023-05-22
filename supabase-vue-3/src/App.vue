af9db8b7e7cacf0ca3da38daba0bad563caa27a6
MichelleO3O <113177083+MichelleO3O@users.noreply.github.com>
2023-05-22 10:19:44 -0400

    imported router

--- supabase-vue-3/src/App.vue
+++ supabase-vue-3/src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from "./views/HelloWorld.vue";
 import GameCard from "./components/GameCard.vue";
 </script>
 
