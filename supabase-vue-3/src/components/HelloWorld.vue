43978eac0c6c97e8aa3d1bc4645d2e9928614235
Vital24 <zynn00001@gmail.com>
2023-05-23 10:15:25 -0400

    Almost done

--- supabase-vue-3/src/components/HelloWorld.vue
+++ supabase-vue-3/src/components/HelloWorld.vue
@@ -1,21 +1,24 @@
-<script setup>
-import { ref } from "vue";
+<script async setup>
+import { ref, watch } from "vue";
 
 const games = ref("");
 const RAWG_API_KEY = "6c361a8e1cbd4e54968bb6859e285e08";
-let page = ref("");
+let page = ref("1");
 
-async function getGames() {
+async function getGames(page) {
   const res = await fetch(
-    `https://api.rawg.io/api/games?dates=2022-01-01%2C2022-12-30&key=${RAWG_API_KEY}&page=${page}&page_size=100&platforms=18%2C1%2C7&ordering=rating`
+    `https://api.rawg.io/api/games?dates=2022-01-01%2C2022-12-30&key=${RAWG_API_KEY}&page=${page.value}&page_size=100&platforms=18%2C1%2C7&ordering=rating`
   );
   const reponse_content = await res.json();
   console.log(reponse_content);
+  return reponse_content;
 }
-getGames();
+
+let gameData = ref("");
+gameData.value = await getGames(page);
 function Jump() {
   page.value++;
-  console.log(page.value);
+
   return page.value;
 }
 function Fall() {
@@ -26,34 +29,19 @@ function Fall() {
     console.log("poop");
   }
   document.querySelector(".backward").disabled = false;
-  console.log(page.value);
+
   return page.value;
 }
+watch(page, (currentValue) => {
+  getGames(Number(currentValue));
+});
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <h1>{{ gameData }}</h1>
   <button class="backward" @click="Fall">◀️</button>
-  <button class="foward" @click="Jump">▶️{{ page }}</button>
-  <div class="card">
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <h3>{{ page }}</h3>
+  <button class="foward" @click="Jump">▶️</button>
 </template>
 
 <style scoped>
