d28fe51cf545e3d696a8c4431c7e8c5737cfb2a0
Vital24 <zynn00001@gmail.com>
2023-05-24 10:16:40 -0400

    ALmost

--- supabase-vue-3/src/components/HelloWorld.vue
+++ supabase-vue-3/src/components/HelloWorld.vue
@@ -4,21 +4,22 @@ import { ref, watch } from "vue";
 const games = ref("");
 const RAWG_API_KEY = "6c361a8e1cbd4e54968bb6859e285e08";
 let page = ref("1");
+let reponse_content = ref("");
+let Link = ref("");
 
 async function getGames(page) {
+  let x = page.value;
   const res = await fetch(
-    `https://api.rawg.io/api/games?dates=2022-01-01%2C2022-12-30&key=${RAWG_API_KEY}&page=${page.value}&page_size=100&platforms=18%2C1%2C7&ordering=rating`
+    `https://api.rawg.io/api/games?dates=2022-01-01%2C2022-12-30&key=${RAWG_API_KEY}&page=${x}&page_size=100&platforms=18%2C1%2C7&ordering=rating`
   );
-  const reponse_content = await res.json();
-  console.log(reponse_content);
-  return reponse_content;
+  Link.value = res.url;
+  reponse_content.value = await res.json();
+  console.log(Link.value);
 }
 
-let gameData = ref("");
-gameData.value = await getGames(page);
 function Jump() {
   page.value++;
-
+  getGames(page);
   return page.value;
 }
 function Fall() {
@@ -26,19 +27,20 @@ function Fall() {
     document.querySelector(".backward").disabled = true;
   } else if (page.value >= 2) {
     page.value--;
-    console.log("poop");
+    getGames(page);
+    return page;
   }
   document.querySelector(".backward").disabled = false;
 
   return page.value;
 }
-watch(page, (currentValue) => {
-  getGames(Number(currentValue));
-});
+/* watch(page, async (currentValue) => {
+  await getGames(Number(currentValue));
+}); */
 </script>
 
 <template>
-  <h1>{{ gameData }}</h1>
+  <h1>1{{ Link }}</h1>
   <button class="backward" @click="Fall">◀️</button>
   <h3>{{ page }}</h3>
   <button class="foward" @click="Jump">▶️</button>
