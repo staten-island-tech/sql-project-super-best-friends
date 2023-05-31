45bb8ca054f5759a8bd9ce422eb304ce601bd38c
Vital24 <zynn00001@gmail.com>
2023-05-31 09:47:13 -0400

    Mistake

--- supabase-vue-3/src/components/HelloWorld.vue
+++ supabase-vue-3/src/components/HelloWorld.vue
@@ -1,10 +1,14 @@
 <script async setup>
+<<<<<<< HEAD
 <<<<<<< HEAD:supabase-vue-3/src/views/HomeView.vue
 import { ref } from "vue";
 import GameCard from "../components/GameCard.vue";
 =======
 import { ref, watch } from "vue";
 >>>>>>> parent of 8b9f340 (Progress?!!???):supabase-vue-3/src/components/HelloWorld.vue
+=======
+import { ref, watch } from "vue";
+>>>>>>> parent of 8b9f340 (Progress?!!???)
 
 const games = ref("");
 const RAWG_API_KEY = "6c361a8e1cbd4e54968bb6859e285e08";
@@ -13,11 +17,15 @@ let reponse_content = ref("");
 
 async function getGames(page) {
   const res = await fetch(
+<<<<<<< HEAD
 <<<<<<< HEAD:supabase-vue-3/src/views/HomeView.vue
     `https://api.rawg.io/api/games?dates=2022-01-01%2C2022-12-30&key=${RAWG_API_KEY}&page=${page.value}&page_size=100&platforms=18%2C1%2C7&ordering=rating-`
 =======
     `https://api.rawg.io/api/games?dates=2022-01-01%2C2022-12-30&key=${RAWG_API_KEY}&page=${page.value}&page_size=100&platforms=18%2C1%2C7&ordering=rating`
 >>>>>>> parent of 8b9f340 (Progress?!!???):supabase-vue-3/src/components/HelloWorld.vue
+=======
+    `https://api.rawg.io/api/games?dates=2022-01-01%2C2022-12-30&key=${RAWG_API_KEY}&page=${page.value}&page_size=100&platforms=18%2C1%2C7&ordering=rating`
+>>>>>>> parent of 8b9f340 (Progress?!!???)
   );
 
   reponse_content.value = await res.json();
@@ -50,6 +58,7 @@ function Fall() {
 </script>
 
 <template>
+<<<<<<< HEAD
 <<<<<<< HEAD:supabase-vue-3/src/views/HomeView.vue
   <h3 v-for="response in response_content.results" :key="name">
     {{ response.name }}
@@ -69,6 +78,11 @@ function Fall() {
     {{ reponse.name }}
   </h3>
 >>>>>>> parent of 8b9f340 (Progress?!!???):supabase-vue-3/src/components/HelloWorld.vue
+=======
+  <h3 v-for="reponse in reponse_content.results" :key="name">
+    {{ reponse.name }}
+  </h3>
+>>>>>>> parent of 8b9f340 (Progress?!!???)
   <button class="backward" @click="Fall">◀️</button>
   <button class="foward" @click="Jump">▶️</button>
 </template>
