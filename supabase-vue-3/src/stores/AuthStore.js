1f0b0a9166ae9c838ea3ffa8410c8899862c1da2
Vital24 <113121145+Vital24@users.noreply.github.com>
2023-06-08 07:11:03 -0400

    asassasas

---
+++ supabase-vue-3/src/stores/AuthStore.js
@@ -0,0 +1,22 @@
+import { defineStore } from "pinia";
+export const AuthStore = defineStore({
+  id: "auth",
+  state: () => {
+    return {
+      currentUser: null,
+    };
+  },
+  actions: {
+    loadUser(user) {
+      this.currentUser = user;
+    },
+    clearUser() {
+      this.currentUser = null;
+    },
+  },
+  getters: {
+    isAuthenticated() {
+      return !!this.currentUser;
+    },
+  },
+});
